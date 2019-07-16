e27fb2654e8edae4158e7624862b39235dc61e6f
kinarashah <kinara@rancher.com>
2019-07-16 13:26:42 -0700

    make v1.15 default

--- rke/k8s_version_info.go
+++ rke/k8s_version_info.go
@@ -4,17 +4,17 @@ import "github.com/rancher/types/apis/management.cattle.io/v3"
 
 func loadRancherDefaultK8sVersions() map[string]string {
 	return map[string]string{
-		"2.3": "v1.14.3-rancher1-1",
+		"2.3": "v1.15.0-rancher1-1",
 		// rancher will use default if its version is absent
-		"default": "v1.14.3-rancher1-1",
+		"default": "v1.15.0-rancher1-1",
 	}
 }
 
 func loadRKEDefaultK8sVersions() map[string]string {
 	return map[string]string{
-		"0.2.3": "v1.14.3-rancher1-1",
+		"0.3": "v1.15.0-rancher1-1",
 		// rke will use default if its version is absent
-		"default": "v1.14.3-rancher1-1",
+		"default": "v1.15.0-rancher1-1",
 	}
 }
 
