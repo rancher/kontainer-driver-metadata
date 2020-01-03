b24b33b73923ad8007e1229a131791930c7b758a
Brenda Rearden <brenda@rancher.com>
2020-01-03 08:54:16 -0700

    v117 windows support

--- rke/k8s_windows_service_options.go
+++ rke/k8s_windows_service_options.go
@@ -5,7 +5,7 @@ import v3 "github.com/rancher/types/apis/management.cattle.io/v3"
 func loadK8sVersionWindowsServiceOptions() map[string]v3.KubernetesServicesOptions {
 	// since 1.14, windows has been supported
 	return map[string]v3.KubernetesServicesOptions{
-		"v1.16": {
+		"v1.17": {
 			Kubelet:   getWindowsKubeletOptions116(),
 			Kubeproxy: getWindowsKubeProxyOptions(),
 		},
