03bca02e992effbf06ee678af1fba6ceecaea7f9
Sebastiaan van Steenis <mail@superseb.nl>
2020-04-10 18:30:50 +0200

    Add support for version checking for templates

--- rke/k8s_defaults.go
+++ rke/k8s_defaults.go
@@ -1,7 +1,6 @@
 package rke
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -22,7 +21,6 @@ const (
 
 var (
 	DriverData     kdm.Data
-	TemplateData   map[string]map[string]string
 	MissedTemplate map[string][]string
 	m              = image.Mirror
 )
@@ -75,14 +73,12 @@ func validateDefaultPresent(versions map[string]string) {
 }
 
 func validateTemplateMatch() {
-	TemplateData = map[string]map[string]string{}
 	MissedTemplate = map[string][]string{}
 	for k8sVersion := range DriverData.K8sVersionRKESystemImages {
 		toMatch, err := semver.Make(k8sVersion[1:])
 		if err != nil {
 			panic(fmt.Sprintf("k8sVersion not sem-ver %s %v", k8sVersion, err))
 		}
-		TemplateData[k8sVersion] = map[string]string{}
 		for plugin, pluginData := range DriverData.K8sVersionedTemplates {
 			if plugin == kdm.TemplateKeys {
 				continue
@@ -104,7 +100,32 @@ func validateTemplateMatch() {
 					matchedRange = toTestRange
 				}
 			}
+
+			// no template found
 			if matchedKey == "" {
+				// check if plugin was introduced later
+				if templateRanges, ok := templates.TemplateIntroducedRanges[plugin]; ok {
+					// as we want to use the logic outside this loop, we check every range and if its matched, we set pluginCheck to true
+					// in the end, we check if any of the ranges have matched, if so, we dont skip the logic to add a missing template (because every version matched in the range should have a template)
+					var pluginCheck bool
+					// plugin has ranges configured
+					for _, toTestRange := range templateRanges {
+						testRange, err := semver.ParseRange(toTestRange)
+						if err != nil {
+							panic(fmt.Sprintf("range for %s not sem-ver %v %v", plugin, testRange, err))
+						}
+						if testRange(toMatch) {
+							pluginCheck = true
+						}
+					}
+					if !pluginCheck {
+						// logrus.Warnf("skipping %s for %s", k8sVersion, plugin)
+						continue
+					}
+
+				}
+
+				// if version not already found for that plugin, append it, else create it
 				if val, ok := MissedTemplate[plugin]; ok {
 					val = append(val, k8sVersion)
 					MissedTemplate[plugin] = val
@@ -113,7 +134,6 @@ func validateTemplateMatch() {
 				}
 				continue
 			}
-			TemplateData[k8sVersion][plugin] = fmt.Sprintf("range=%s key=%s", matchedRange, matchedKey)
 		}
 	}
 }
@@ -130,16 +150,6 @@ func readFile(input string, data map[string]interface{}) error {
 func GenerateData() {
 	initData()
 
-	buf := new(bytes.Buffer)
-	enc := json.NewEncoder(buf)
-	enc.SetEscapeHTML(false)
-	enc.SetIndent("", " ")
-
-	if err := enc.Encode(TemplateData); err != nil {
-		panic(fmt.Sprintf("error encoding template data %v", err))
-	}
-	fmt.Println(buf.String())
-
 	if len(MissedTemplate) != 0 {
 		logrus.Warnf("found k8s versions without a template")
 		for plugin, data := range MissedTemplate {
@@ -147,7 +157,6 @@ func GenerateData() {
 		}
 	}
 
-	fmt.Println("generating data.json")
 	//todo: zip file
 	strData, _ := json.MarshalIndent(DriverData, "", " ")
 	jsonFile, err := os.Create(rkeDataFilePath)
@@ -159,4 +168,5 @@ func GenerateData() {
 	if err != nil {
 		panic(fmt.Errorf("err writing jsonFile %v", err))
 	}
+	fmt.Println("finished generating data.json")
 }
