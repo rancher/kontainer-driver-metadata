ad5f4d48261444998b156904b90462e771b12d39
rajashree <rajashree.28m@gmail.com>
2020-04-17 13:30:18 -0700

    New 1.16 and 1.17 patch changes

--- rke/k8s_rke_system_images.go
+++ rke/k8s_rke_system_images.go
@@ -2336,11 +2336,11 @@ func loadK8sRKESystemImages() map[string]v3.RKESystemImages {
 			WindowsPodInfraContainer:  m("rancher/kubelet-pause:v0.1.3"),
 			Nodelocal:                 m("k8s.gcr.io/k8s-dns-node-cache:1.15.7"),
 		},
-		// Enabled in Rancher v2.3.7/v2.4.0
+		// Enabled in Rancher v2.3.7/v2.4.3
 		// Reminder: This template contains Nodelocal image which isn't in templates before v2.3.6
-		"v1.16.8-rancher1-3": {
+		"v1.16.9-rancher1-1": {
 			Etcd:                      m("quay.io/coreos/etcd:v3.3.15-rancher1"),
-			Kubernetes:                m("rancher/hyperkube:v1.16.8-rancher1"),
+			Kubernetes:                m("rancher/hyperkube:v1.16.9-rancher1"),
 			Alpine:                    m("rancher/rke-tools:v0.1.56"),
 			NginxProxy:                m("rancher/rke-tools:v0.1.56"),
 			CertDownloader:            m("rancher/rke-tools:v0.1.56"),
@@ -2634,11 +2634,11 @@ func loadK8sRKESystemImages() map[string]v3.RKESystemImages {
 			WindowsPodInfraContainer:  m("rancher/kubelet-pause:v0.1.3"),
 			Nodelocal:                 m("k8s.gcr.io/k8s-dns-node-cache:1.15.7"),
 		},
-		// Enabled in Rancher v2.3.7/v2.4.0
+		// Enabled in Rancher v2.3.7/v2.4.3
 		// Reminder: This template contains Nodelocal image which isn't in templates before v2.3.6
-		"v1.17.4-rancher1-3": {
+		"v1.17.5-rancher1-1": {
 			Etcd:                      m("quay.io/coreos/etcd:v3.4.3-rancher1"),
-			Kubernetes:                m("rancher/hyperkube:v1.17.4-rancher1"),
+			Kubernetes:                m("rancher/hyperkube:v1.17.5-rancher1"),
 			Alpine:                    m("rancher/rke-tools:v0.1.56"),
 			NginxProxy:                m("rancher/rke-tools:v0.1.56"),
 			CertDownloader:            m("rancher/rke-tools:v0.1.56"),
