f63a2197fe94940a9b48ed9318e92f1584f79801
kinarashah <kinara@rancher.com>
2020-10-26 11:22:51 -0700

    base copy for v1.15

--- rke/k8s_version_info.go
+++ rke/k8s_version_info.go
@@ -130,6 +130,13 @@ func loadK8sVersionInfo() map[string]v3.K8sVersionInfo {
 			MinRKEVersion:     "1.1.5-rc0",
 		},
 		// The Calico/Canal template in this version use functions that are only available in RKE v1.0.0 and up
+		// This version includes nodelocal dns only available in RKE v1.0.7 and up
+		// It also includes ingress-nginx 0.32.0
+		"v1.15.12-rancher2-6": {
+			MinRancherVersion: "2.4.6-rc0",
+			MinRKEVersion:     "1.1.5-rc0",
+		},
+		// The Calico/Canal template in this version use functions that are only available in RKE v1.0.0 and up
 		"v1.16.8-rancher1-1": {
 			MinRancherVersion: "2.3.3",
 			MinRKEVersion:     "1.0.0",
