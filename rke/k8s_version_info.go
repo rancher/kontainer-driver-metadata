c499d4367e8ceb10e48fdc3db49f389b496abfd3
Daishan <daishan@rancher.com>
2021-01-30 13:26:14 -0700

    Add k8s v1.20.2-rancher2-1

--- rke/k8s_version_info.go
+++ rke/k8s_version_info.go
@@ -389,6 +389,10 @@ func loadK8sVersionInfo() map[string]v3.K8sVersionInfo {
 			MinRancherVersion: "2.5.0-rc0",
 			MinRKEVersion:     "1.2.0-rc0",
 		},
+		"v1.20": {
+			MinRancherVersion: "2.5.6-rc0",
+			MinRKEVersion:     "1.2.0-rc0",
+		},
 		"v1.8.10-rancher1-1": {
 			DeprecateRKEVersion:     "0.2.2",
 			DeprecateRancherVersion: "2.2",
