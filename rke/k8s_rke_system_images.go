cd10f5c5bd2bff3d1caccc2ac0bb091d7f5181fd
Kinara Shah <kinara@rancher.com>
2021-03-10 09:46:18 -0800

    new versions for mirror images

--- rke/k8s_rke_system_images.go
+++ rke/k8s_rke_system_images.go
@@ -4092,10 +4092,10 @@ func loadK8sRKESystemImages() map[string]v3.RKESystemImages {
 			WindowsPodInfraContainer:  m("rancher/kubelet-pause:v0.1.6"),
 			Nodelocal:                 m("k8s.gcr.io/k8s-dns-node-cache:1.15.7"),
 		},
-		// Enabled in v2.5.6
+		// Enabled out of band post v2.5.6
 		// Reminder: This template contains Nodelocal image which isn't in templates before v2.3.6
 		// Reminder: This template contains ACI images which aren't in templates for v2.4.x
-		"v1.18.16-rancher1-1": {
+		"v1.18.16-rancher1-2": {
 			Etcd:                      m("rancher/coreos-etcd:v3.4.3-rancher1"),
 			Kubernetes:                m("rancher/hyperkube:v1.18.16-rancher1"),
 			Alpine:                    m("rancher/rke-tools:v0.1.72"),
@@ -4408,9 +4408,9 @@ func loadK8sRKESystemImages() map[string]v3.RKESystemImages {
 			WindowsPodInfraContainer:  m("rancher/kubelet-pause:v0.1.6"),
 			Nodelocal:                 m("k8s.gcr.io/k8s-dns-node-cache:1.15.13"),
 		},
-		// Enabled in v2.5.6
+		// Enabled out of band post v2.5.6
 		// Reminder: This template contains ACI images which aren't in templates for v2.4.x
-		"v1.19.8-rancher1-1": {
+		"v1.19.8-rancher1-2": {
 			Etcd:                      m("rancher/coreos-etcd:v3.4.13-rancher1"),
 			Kubernetes:                m("rancher/hyperkube:v1.19.8-rancher1"),
 			Alpine:                    m("rancher/rke-tools:v0.1.72"),
@@ -4496,9 +4496,9 @@ func loadK8sRKESystemImages() map[string]v3.RKESystemImages {
 			WindowsPodInfraContainer:  m("rancher/kubelet-pause:v0.1.6"),
 			Nodelocal:                 m("k8s.gcr.io/k8s-dns-node-cache:1.15.13"),
 		},
-		// Enabled in v2.5.6
+		// Enabled out of band post v2.5.6
 		// Reminder: This template contains ACI images which aren't in templates for v2.4.x
-		"v1.20.4-rancher1-1": {
+		"v1.20.4-rancher1-2": {
 			Etcd:                      m("rancher/coreos-etcd:v3.4.14-rancher1"),
 			Kubernetes:                m("rancher/hyperkube:v1.20.4-rancher1"),
 			Alpine:                    m("rancher/rke-tools:v0.1.72"),
