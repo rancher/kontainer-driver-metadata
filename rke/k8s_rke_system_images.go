30309d76b1fc3bdc285c109c1b69ab1f4dff10f2
Kinara Shah <kinara@rancher.com>
2021-06-02 11:10:44 -0700

    add v1.18.19

--- rke/k8s_rke_system_images.go
+++ rke/k8s_rke_system_images.go
@@ -4371,13 +4371,13 @@ func loadK8sRKESystemImages() map[string]v3.RKESystemImages {
 		// Enabled out of band post v2.4.15
 		// Reminder: This template contains Nodelocal image which isn't in templates before v2.3.6
 		// Reminder: This template should remain an exact copy of the template for v2.5.x (No ACI images)
-		"v1.18.18-rancher1-1": {
+		"v1.18.19-rancher1-1": {
 			Etcd:                      m("rancher/coreos-etcd:v3.4.15-rancher1"),
-			Kubernetes:                m("rancher/hyperkube:v1.18.18-rancher1"),
-			Alpine:                    m("rancher/rke-tools:v0.1.74"),
-			NginxProxy:                m("rancher/rke-tools:v0.1.74"),
-			CertDownloader:            m("rancher/rke-tools:v0.1.74"),
-			KubernetesServicesSidecar: m("rancher/rke-tools:v0.1.74"),
+			Kubernetes:                m("rancher/hyperkube:v1.18.19-rancher1"),
+			Alpine:                    m("rancher/rke-tools:v0.1.75"),
+			NginxProxy:                m("rancher/rke-tools:v0.1.75"),
+			CertDownloader:            m("rancher/rke-tools:v0.1.75"),
+			KubernetesServicesSidecar: m("rancher/rke-tools:v0.1.75"),
 			KubeDNS:                   m("gcr.io/google_containers/k8s-dns-kube-dns:1.15.2"),
 			DNSmasq:                   m("gcr.io/google_containers/k8s-dns-dnsmasq-nanny:1.15.2"),
 			KubeDNSSidecar:            m("gcr.io/google_containers/k8s-dns-sidecar:1.15.2"),
