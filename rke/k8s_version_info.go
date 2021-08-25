facd3262e0ed5148441ca2e9ab2a9077d4ad8467
Kinara Shah <kinara@rancher.com>
2021-08-25 13:54:59 -0700

    remove v1.19.13-rancher1-2, v1.19.14 already exists

--- rke/k8s_version_info.go
+++ rke/k8s_version_info.go
@@ -486,10 +486,6 @@ func loadK8sVersionInfo() map[string]v3.K8sVersionInfo {
 			MinRancherVersion: "2.5.0-rc0",
 			MinRKEVersion:     "1.2.0-rc0",
 		},
-		"v1.19.13-rancher1-2": {
-			MinRancherVersion: "2.5.0-rc0",
-			MinRKEVersion:     "1.2.0-rc0",
-		},
 		"v1.19.14-rancher1-1": {
 			MinRancherVersion: "2.5.0-rc0",
 			MinRKEVersion:     "1.2.0-rc0",
