226f4538d35f68350a5bb5f0fd27156f7b90275d
Arvind Iyengar <arvind.iyengar@rancher.com>
2021-12-02 17:52:28 -0800

    Base copy

--- rke/k8s_version_info.go
+++ rke/k8s_version_info.go
@@ -358,6 +358,12 @@ func loadK8sVersionInfo() map[string]v3.K8sVersionInfo {
 		},
 		// The Calico/Canal template in this version use functions that are only available in RKE v1.0.0 and up
 		// This version includes nodelocal dns only available in RKE v1.0.7 and up
+		"v1.17.17-rancher2-3": {
+			MinRancherVersion: "2.4.4-rc0",
+			MinRKEVersion:     "1.1.2-rc0",
+		},
+		// The Calico/Canal template in this version use functions that are only available in RKE v1.0.0 and up
+		// This version includes nodelocal dns only available in RKE v1.0.7 and up
 		"v1.18.3-rancher2-1": {
 			MinRancherVersion: "2.4.5-rc0",
 			MinRKEVersion:     "1.1.3-rc0",
@@ -470,6 +476,12 @@ func loadK8sVersionInfo() map[string]v3.K8sVersionInfo {
 			MinRancherVersion: "2.4.5-rc0",
 			MinRKEVersion:     "1.1.3-rc0",
 		},
+		// The Calico/Canal template in this version use functions that are only available in RKE v1.0.0 and up
+		// This version includes nodelocal dns only available in RKE v1.0.7 and up
+		"v1.18.20-rancher1-2": {
+			MinRancherVersion: "2.4.5-rc0",
+			MinRKEVersion:     "1.1.3-rc0",
+		},
 		"v1.17": {
 			MaxRancherVersion: "2.5.99",
 			MaxRKEVersion:     "1.2.99",
@@ -506,6 +518,10 @@ func loadK8sVersionInfo() map[string]v3.K8sVersionInfo {
 			MinRancherVersion: "2.5.11-rc0",
 			MinRKEVersion:     "1.2.14-rc0",
 		},
+		"v1.19.16-rancher1-1": {
+			MinRancherVersion: "2.5.11-rc0",
+			MinRKEVersion:     "1.2.14-rc0",
+		},
 		"v1.20": {
 			MinRancherVersion: "2.5.6-rc0",
 			MinRKEVersion:     "1.2.0-rc0",
@@ -529,6 +545,10 @@ func loadK8sVersionInfo() map[string]v3.K8sVersionInfo {
 		"v1.20.11-rancher1-2": {
 			MinRancherVersion: "2.5.6-rc0",
 			MinRKEVersion:     "1.2.0-rc0",
+		},
+		"v1.20.12-rancher1-1": {
+			MinRancherVersion: "2.5.11-rc0",
+			MinRKEVersion:     "1.2.14-rc0",
 		},
 		"v1.20.12-rancher1-1": {
 			MinRancherVersion: "2.5.11-rc0",
