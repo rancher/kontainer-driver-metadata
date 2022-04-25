887478b8917c235ffda007a44e47c67becd45278
Kinara Shah <kinara@rancher.com>
2022-04-25 21:10:31 +0530

    add nginx v1.20

--- rke/k8s_version_info.go
+++ rke/k8s_version_info.go
@@ -533,6 +533,14 @@ func loadK8sVersionInfo() map[string]v3.K8sVersionInfo {
 			MinRancherVersion: "2.5.11-rc0",
 			MinRKEVersion:     "1.2.14-rc0",
 		},
+		"v1.19.16-rancher1-4": {
+			MinRancherVersion: "2.5.11-rc0",
+			MinRKEVersion:     "1.2.14-rc0",
+		},
+		"v1.19.16-rancher1-5": {
+			MinRancherVersion: "2.5.11-rc0",
+			MinRKEVersion:     "1.2.14-rc0",
+		},
 		"v1.20": {
 			MinRancherVersion: "2.5.6-rc0",
 			MinRKEVersion:     "1.2.0-rc0",
@@ -578,6 +586,14 @@ func loadK8sVersionInfo() map[string]v3.K8sVersionInfo {
 			MinRKEVersion:     "1.2.14-rc0",
 		},
 		"v1.20.15-rancher1-1": {
+			MinRancherVersion: "2.5.11-rc0",
+			MinRKEVersion:     "1.2.14-rc0",
+		},
+		"v1.20.15-rancher1-2": {
+			MinRancherVersion: "2.5.11-rc0",
+			MinRKEVersion:     "1.2.14-rc0",
+		},
+		"v1.20.15-rancher1-3": {
 			MinRancherVersion: "2.5.11-rc0",
 			MinRKEVersion:     "1.2.14-rc0",
 		},
