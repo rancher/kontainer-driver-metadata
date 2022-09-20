0f969e29e3968308c602238508a14bb7eec338dc
Rayan Das <rayandas91@gmail.com>
2022-09-20 18:16:26 +0530

    add a new template metrics_v0.5.1-fix.go to introduce allowPrivilegeEscalation: false

--- rke/templates/templates.go
+++ rke/templates/templates.go
@@ -55,10 +55,11 @@ const (
 	kubeDnsv18  = "kubedns-v1.8"
 	kubeDnsv116 = "kubedns-v1.16"
 
-	metricsServerv18  = "metricsserver-v1.8"
-	metricsServerv120 = "metricsserver-v1.20"
-	metricsServerv050 = "metricsserver-v0.5.0"
-	metricsServerv061 = "metricsserver-v0.6.1"
+	metricsServerv18     = "metricsserver-v1.8"
+	metricsServerv120    = "metricsserver-v1.20"
+	metricsServerv050    = "metricsserver-v0.5.0"
+	metricsServerv051fix = "metricsserver-v0.5.1-fix"
+	metricsServerv061    = "metricsserver-v0.6.1"
 
 	weavev18  = "weave-v1.8"
 	weavev116 = "weave-v1.16"
@@ -156,10 +157,11 @@ func LoadK8sVersionedTemplates() map[string]map[string]string {
 			">=1.8.0-rancher0 <1.16.0-alpha": kubeDnsv18,
 		},
 		kdm.MetricsServer: {
-			">=1.23.3-rancher1-1":                     metricsServerv061,
-			">=1.20.14-rancher2-1 <1.23.3-rancher1-1": metricsServerv050,
-			">=1.20.4-rancher1-1 <1.20.14-rancher2-1": metricsServerv120,
-			">=1.8.0-rancher0 <1.20.4-rancher1-1":     metricsServerv18,
+			">=1.23.3-rancher1-1":                      metricsServerv061,
+			">=1.22.15-rancher1-1 <1.23.3-rancher1-1":  metricsServerv051fix,
+			">=1.20.14-rancher2-1 <1.22.15-rancher1-1": metricsServerv050,
+			">=1.20.4-rancher1-1 <1.20.14-rancher2-1":  metricsServerv120,
+			">=1.8.0-rancher0 <1.20.4-rancher1-1":      metricsServerv18,
 		},
 		kdm.Weave: {
 			">=1.22.0-rancher1-1":                    weavev122,
@@ -267,10 +269,11 @@ func getTemplates() map[string]string {
 		kubeDnsv18:  KubeDNSTemplate,
 		kubeDnsv116: KubeDNSTemplateV116,
 
-		metricsServerv18:  MetricsServerTemplate,
-		metricsServerv120: MetricsServerTemplateV0_4_1,
-		metricsServerv050: MetricsServerTemplateV0_5_0,
-		metricsServerv061: MetricsServerTemplateV0_6_1,
+		metricsServerv18:     MetricsServerTemplate,
+		metricsServerv120:    MetricsServerTemplateV0_4_1,
+		metricsServerv050:    MetricsServerTemplateV0_5_0,
+		metricsServerv051fix: MetricsServerTemplateV0_5_1_fix,
+		metricsServerv061:    MetricsServerTemplateV0_6_1,
 
 		weavev18:  WeaveTemplate,
 		weavev116: WeaveTemplateV116,
