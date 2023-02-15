2674666b4fc7fdf8fb2c1d534e333f56858adf45
Rayan Das <rayandas91@gmail.com>
2023-02-15 10:54:49 +0530

    bump hyperkube tag in jan patches which are still not released.

--- rke/k8s_rke_system_images.go
+++ rke/k8s_rke_system_images.go
@@ -8331,9 +8331,9 @@ func loadK8sRKESystemImages() map[string]v3.RKESystemImages {
 			Nodelocal:                 "rancher/mirrored-k8s-dns-node-cache:1.21.1",
 		},
 		// Enabled out of band post v2.6.11
-		"v1.23.16-rancher1-1": {
+		"v1.23.16-rancher2-1": {
 			Etcd:                      "rancher/mirrored-coreos-etcd:v3.5.3",
-			Kubernetes:                "rancher/hyperkube:v1.23.16-rancher1",
+			Kubernetes:                "rancher/hyperkube:v1.23.16-rancher2",
 			Alpine:                    "rancher/rke-tools:v0.1.88",
 			NginxProxy:                "rancher/rke-tools:v0.1.88",
 			CertDownloader:            "rancher/rke-tools:v0.1.88",
@@ -8595,9 +8595,9 @@ func loadK8sRKESystemImages() map[string]v3.RKESystemImages {
 			Nodelocal:                 "rancher/mirrored-k8s-dns-node-cache:1.21.1",
 		},
 		// Enabled out of band post v2.6.11
-		"v1.24.10-rancher1-1": {
+		"v1.24.10-rancher2-1": {
 			Etcd:                      "rancher/mirrored-coreos-etcd:v3.5.4",
-			Kubernetes:                "rancher/hyperkube:v1.24.10-rancher1",
+			Kubernetes:                "rancher/hyperkube:v1.24.10-rancher2",
 			Alpine:                    "rancher/rke-tools:v0.1.88",
 			NginxProxy:                "rancher/rke-tools:v0.1.88",
 			CertDownloader:            "rancher/rke-tools:v0.1.88",
