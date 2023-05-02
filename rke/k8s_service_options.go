1edd0a773ffdc9f6db4d1257abe6ad5b647b4c55
vardhaman <vardhaman.surana@suse.com>
2023-05-02 18:43:40 +0530

    added k8s 1.26.4-rancher1

--- rke/k8s_service_options.go
+++ rke/k8s_service_options.go
@@ -15,6 +15,14 @@ const (
 
 func loadK8sVersionServiceOptions() map[string]v3.KubernetesServicesOptions {
 	return map[string]v3.KubernetesServicesOptions{
+		"v1.26": {
+			Etcd:           getETCDOptions122(),
+			KubeAPI:        getKubeAPIOptions124(),
+			Kubelet:        getKubeletOptions124(),
+			KubeController: getKubeControllerOptions124(),
+			Kubeproxy:      getKubeProxyOptions(),
+			Scheduler:      getSchedulerOptions124(),
+		},
 		"v1.25": {
 			Etcd:           getETCDOptions122(),
 			KubeAPI:        getKubeAPIOptions124(),
