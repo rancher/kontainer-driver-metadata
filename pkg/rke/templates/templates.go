607b47c8ff85b1ce0c6c53974bb937b37f65a50d
vardhaman22 <vardhaman.surana@suse.com>
2023-10-27 16:18:37 +0530

    added nginx v1.9.4 template

--- pkg/rke/templates/templates.go
+++ pkg/rke/templates/templates.go
@@ -101,6 +101,7 @@ const (
 	nginxIngressv120Rancher2 = "nginxingress-v1.2.0-rancher2"
 	nginxIngressv151Rancher2 = "nginxingress-v1.5.1-rancher2"
 	nginxIngressv170Rancher1 = "nginxingress-v1.7.0-rancher1"
+	nginxIngressv194Rancher1 = "nginxingress-v1.9.4-rancher1"
 
 	nodelocalv115 = "nodelocal-v1.15"
 	nodelocalv121 = "nodelocal-v1.21"
@@ -275,8 +276,10 @@ func LoadK8sVersionedTemplates() map[string]map[string]string {
 			">=1.23.6-rancher1-1 <1.23.16-rancher1-1":  nginxIngressv120Rancher2,
 			">=1.23.16-rancher1-1 <1.24.0-rancher1-1":  nginxIngressv151Rancher2,
 			">=1.24.2-rancher1-1 <1.24.10-rancher1-1":  nginxIngressv120Rancher2,
-			">=1.24.10-rancher1-1 <1.26.0-rancher1-1":  nginxIngressv151Rancher2,
-			">=1.26.0-rancher1-1":                      nginxIngressv170Rancher1,
+			">=1.24.10-rancher1-1 <1.25.16-rancher1-1": nginxIngressv151Rancher2,
+			">=1.25.16-rancher1-1 <1.26.0-rancher1-1":  nginxIngressv194Rancher1,
+			">=1.26.0-rancher1-1 <1.26.11-rancher1-1":  nginxIngressv170Rancher1,
+			">=1.26.11-rancher1-1":                     nginxIngressv194Rancher1,
 		},
 		kdm.Nodelocal: {
 			">=1.15.11-rancher0 <1.16.0-alpha":     nodelocalv115,
@@ -377,6 +380,7 @@ func getTemplates() map[string]string {
 		nginxIngressv120Rancher2: NginxIngressTemplateV120Rancher2,
 		nginxIngressv151Rancher2: NginxIngressTemplateV151Rancher2,
 		nginxIngressv170Rancher1: NginxIngressTemplateV170Rancher1,
+		nginxIngressv194Rancher1: NginxIngressTemplateV194Rancher1,
 
 		nodelocalv115: NodelocalTemplateV115,
 		nodelocalv121: NodelocalTemplateV121,
