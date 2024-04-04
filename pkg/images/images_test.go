f15da3169f884b04d13383e07bad3091cbb3a064
Kinara Shah <kinara@rancher.com>
2024-04-03 17:46:26 -0700

    update server versions for v2.9.0

--- pkg/images/images_test.go
+++ pkg/images/images_test.go
@@ -99,8 +99,8 @@ func Test_toKeep(t *testing.T) {
 		{
 			name: "min < 2.7.0 < max < 2.9.0",
 			args: args{info: types.K8sVersionInfo{
-				MinRancherVersion:       "2.6.12-alpha2",
-				MaxRancherVersion:       "2.8.12-patch1",
+				MinRancherVersion:       "2.7.12-alpha2",
+				MaxRancherVersion:       "2.9.12-patch1",
 				DeprecateRancherVersion: "",
 			}},
 			want:    true,
@@ -130,17 +130,17 @@ func Test_toKeep(t *testing.T) {
 			name: "2.7.0 < min < max < 2.9.0",
 			args: args{info: types.K8sVersionInfo{
 				MinRancherVersion:       "2.7.1-patch-1",
-				MaxRancherVersion:       "2.8.5",
+				MaxRancherVersion:       "2.9.5",
 				DeprecateRancherVersion: "",
 			}},
 			want:    true,
 			wantErr: false,
 		},
 		{
-			name: "2.9.0 < min < 2.9.99 < max",
+			name: "2.10.0 < min < 2.10.99 < max",
 			args: args{info: types.K8sVersionInfo{
-				MinRancherVersion:       "2.9.1-patch1",
-				MaxRancherVersion:       "2.10.0-alpha1",
+				MinRancherVersion:       "2.10.1-patch1",
+				MaxRancherVersion:       "2.11.0-alpha1",
 				DeprecateRancherVersion: "",
 			}},
 			want:    false,
@@ -194,8 +194,8 @@ func Test_releaseToKeep(t *testing.T) {
 		{
 			name: "min < 2.7.0 < 2.9.0 < max",
 			args: args{release: map[string]interface{}{
-				"minChannelServerVersion": "v2.6.0-alpha1",
-				"maxChannelServerVersion": "v2.9.1",
+				"minChannelServerVersion": "v2.7.0-alpha1",
+				"maxChannelServerVersion": "v2.9.9",
 				"version":                 "v1.20.15+rke2r2"},
 			},
 			want:    true,
@@ -204,8 +204,8 @@ func Test_releaseToKeep(t *testing.T) {
 		{
 			name: "min < 2.7.0 < max = 2.9.0",
 			args: args{release: map[string]interface{}{
-				"minChannelServerVersion": "v2.6.99",
-				"maxChannelServerVersion": "v2.9.0",
+				"minChannelServerVersion": "v2.7.99",
+				"maxChannelServerVersion": "v2.10.0",
 				"version":                 "v1.20.15+rke2r2"},
 			},
 			want:    true,
@@ -215,7 +215,7 @@ func Test_releaseToKeep(t *testing.T) {
 			name: "min < 2.7.0 < max < 2.9.0",
 			args: args{release: map[string]interface{}{
 				"minChannelServerVersion": "v2.6.12-alpha1",
-				"maxChannelServerVersion": "v2.8.99",
+				"maxChannelServerVersion": "v2.9.99",
 				"version":                 "v1.20.15+rke2r2"},
 			},
 			want:    true,
@@ -226,7 +226,7 @@ func Test_releaseToKeep(t *testing.T) {
 			name: "min = 2.7.0 < max < 2.9.0",
 			args: args{release: map[string]interface{}{
 				"minChannelServerVersion": "v2.7.0",
-				"maxChannelServerVersion": "v2.8.99",
+				"maxChannelServerVersion": "v2.9.99",
 				"version":                 "v1.20.15+rke2r2"},
 			},
 			want:    true,
@@ -236,7 +236,7 @@ func Test_releaseToKeep(t *testing.T) {
 			name: "min = 2.7.0 < max = 2.9.0",
 			args: args{release: map[string]interface{}{
 				"minChannelServerVersion": "v2.7.0",
-				"maxChannelServerVersion": "v2.9.0",
+				"maxChannelServerVersion": "v2.10.0",
 				"version":                 "v1.20.15+rke2r2"},
 			},
 			want:    true,
@@ -246,17 +246,17 @@ func Test_releaseToKeep(t *testing.T) {
 			name: "2.7.0 < min < max < 2.9.0",
 			args: args{release: map[string]interface{}{
 				"minChannelServerVersion": "v2.7.2-alpha1",
-				"maxChannelServerVersion": "v2.8.14-patch1",
+				"maxChannelServerVersion": "v2.9.14-patch1",
 				"version":                 "v1.20.15+rke2r2"},
 			},
 			want:    true,
 			wantErr: false,
 		},
 		{
-			name: "2.9.0 < min < 2.9.99 < max",
+			name: "2.10.0 < min < 2.10.99 < max",
 			args: args{release: map[string]interface{}{
-				"minChannelServerVersion": "v2.9.1-alpha1",
-				"maxChannelServerVersion": "v2.10.0-patch1",
+				"minChannelServerVersion": "v2.10.1-alpha1",
+				"maxChannelServerVersion": "v2.11.0-patch1",
 				"version":                 "v1.20.15+rke2r2"},
 			},
 			want:    false,
@@ -349,7 +349,7 @@ func Test_unique(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			err := unique(tt.args.imageTag, tt.args.images)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("unique() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("unique() error = %v, wantErr %v %v", err, tt.wantErr, tt.args)
 				return
 			}
 		})
