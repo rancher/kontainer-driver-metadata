6430a031c0db929889ded5f8ccb6ed2ff161a790
mitulshah-suse <mitul.shah@suse.com>
2024-07-04 04:01:33 +0530

    Move from drone to Github Actions for 2.9 (#1402)

--- scripts/test-run-required.sh
+++ scripts/test-run-required.sh
@@ -2,7 +2,7 @@
 set -ex
 
 echo "Checking if rancher integration testing is required"
-echo "Environment variable DRONE_BUILD_EVENT is ${DRONE_BUILD_EVENT}"
+echo "Environment variable GITHUB_EVENT_NAME is ${GITHUB_EVENT_NAME}"
 
 if [ -z "$CI" ]; then
   echo "Not running in CI, rancher integration testing is required"
@@ -14,15 +14,15 @@ if [ -z "$KDM_TEST_K8S_MINOR" ]; then
   exit 1
 fi
 
-if [ -z "$DRONE_COMMIT_BEFORE" ]; then
-  echo "Error: DRONE_COMMIT_BEFORE not defined. This should not be happening in CI"
+if [ -z "$PREV_COMMIT_SHA" ]; then
+  echo "Error: PREV_COMMIT_SHA not defined. This should not be happening in CI"
   exit 1
 fi
 
-# Only run check if Drone build event is 'push' or 'pull_request'
-if [ "${DRONE_BUILD_EVENT}" = "push" ] || [ "${DRONE_BUILD_EVENT}" = "pull_request" ]; then
+# Only run check if Github build event is 'push' or 'pull_request'
+if [ "${GITHUB_EVENT_NAME}" = "push" ] || [ "${GITHUB_EVENT_NAME}" = "pull_request" ]; then
   # Check if the channels file contains changes to versions from the minor version
-  if [ "$(git --no-pager diff --no-color -G "^  - version:" $DRONE_COMMIT_BEFORE -- "$CHANNELS_FILE" | grep -c -P "(^\+\s+- version: v1.$KDM_TEST_K8S_MINOR)")" -ne 0 ]; then
+  if [ "$(git --no-pager diff --no-color -G "^  - version:" $PREV_COMMIT_SHA -- "$CHANNELS_FILE" | grep -c -P "(^\+\s+- version: v1.$KDM_TEST_K8S_MINOR)")" -ne 0 ]; then
     exit 0
   fi
 fi
