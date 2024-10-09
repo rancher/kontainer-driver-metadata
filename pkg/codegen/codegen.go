d0bebf480c9145d490c9683559ac83be20734e86
Kinara Shah <kinara@rancher.com>
2024-10-09 10:18:42 -0600

    test removing windows

--- pkg/codegen/codegen.go
+++ pkg/codegen/codegen.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"github.com/rancher/kontainer-driver-metadata/pkg/images"
 	"github.com/rancher/kontainer-driver-metadata/pkg/rke"
 )
 
 func main() {
 	// add drivers init here
 	rke.GenerateData()
-	images.GenerateRegSyncFile()
+	//images.GenerateRegSyncFile()
 }
