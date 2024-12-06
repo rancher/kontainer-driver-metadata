a8bb983cace3c48cd2e7c73678b0a8de0a420ac3
santosh kumar pandranki <santosh.pandranki@oneconvergence.com>
2024-12-06 08:10:26 -0800

    [v2.9] Add ACI CNI 6.0.4.4 template changes (#1566)
    * Add ACI-CNI 6.0.4.4 template changes
    
    Co-authored-by: Akhilesh Sarda <akhilesh.sarda@oneconvergence.com>

--- pkg/rke/templates/templates.go
+++ pkg/rke/templates/templates.go
@@ -111,6 +111,7 @@ const (
 	aciv6042 = "aci-v6.0.4.2"
 	aciv6043 = "aci-v6.0.4.3"
 	aciv6111 = "aci-v6.1.1.1"
+	aciv6044 = "aci-v6.0.4.4"
 
 	nginxIngressv18           = "nginxingress-v1.8"
 	nginxIngressV115          = "nginxingress-v1.15"
@@ -313,10 +314,12 @@ func LoadK8sVersionedTemplates() map[string]map[string]string {
 			">=1.28.14-rancher1-1 < 1.29.0-rancher0":    aciv6111,
 			">=1.29.0-rancher0 < 1.29.8-rancher1-1":     aciv6042,
 			">=1.29.8-rancher1-1 < 1.29.9-rancher1-1":   aciv6043,
-			">=1.29.9-rancher1-1 < 1.30.0-rancher0":     aciv6111,
+			">=1.29.9-rancher1-1 < 1.29.11-rancher1-1":  aciv6111,
+			">=1.29.11-rancher1-1 < 1.30.0-rancher0":    aciv6044,
 			">=1.30.0-rancher0 < 1.30.4-rancher1-1":     aciv6042,
 			">=1.30.4-rancher1-1 < 1.30.5-rancher1-1":   aciv6043,
-			">=1.30.5-rancher1-1":                       aciv6111,
+			">=1.30.5-rancher1-1 < 1.30.7-rancher1-1":   aciv6111,
+			">=1.30.7-rancher1-1":                       aciv6044,
 		},
 		kdm.NginxIngress: {
 			">=1.8.0-rancher0 <1.13.10-rancher1-3":  nginxIngressv18,
@@ -481,6 +484,7 @@ func getTemplates() map[string]string {
 		aciv6042: AciTemplateV6042,
 		aciv6043: AciTemplateV6043,
 		aciv6111: AciTemplateV6111,
+		aciv6044: AciTemplateV6044,
 
 		nginxIngressv18:           NginxIngressTemplate,
 		nginxIngressV115:          NginxIngressTemplateV0251Rancher1,
