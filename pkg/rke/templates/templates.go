3fc2d177fc6962f83f95fdecf0a63540579e043a
Krunal Hingu <krunal.hingu222@gmail.com>
2025-09-16 09:25:55 +0530

    [dev-v2.11-rke-extended-life] Bump flannel v0.27.3 (#1780)
    * bump flannel v0.27.3
    
    * add template for flannel v0.27.3
    
    * fix: version mismatch
    
    * go generate
    
    Signed-off-by: swastik959 <swastik.gour@suse.com>
    
    * corrected templates
    
    Signed-off-by: swastik959 <swastik.gour@suse.com>
    
    ---------
    
    Signed-off-by: swastik959 <swastik.gour@suse.com>
    Co-authored-by: swastik959 <swastik.gour@suse.com>

--- pkg/rke/templates/templates.go
+++ pkg/rke/templates/templates.go
@@ -77,6 +77,7 @@ const (
 	flannelv0242         = "flannel-v0.24.2"
 	flannelv0251         = "flannel-v0.25.1"
 	flannelv0264         = "flannel-v0.26.4"
+	flannelv0273         = "flannel-v0.27.3"
 
 	coreDnsv18          = "coredns-v1.8"
 	coreDnsv116         = "coredns-v1.16"
@@ -245,8 +246,10 @@ func LoadK8sVersionedTemplates() map[string]map[string]string {
 			">=1.8.0-rancher0 <1.13.0-rancher0":        canalv18,
 		},
 		kdm.Flannel: {
-			">=1.32.0-rancher1-1":                     flannelv0264,
-			">=1.29.0-rancher1-1 <1.32.0-rancher1-1":  flannelv0251,
+			">=1.32.8-rancher1-1":                     flannelv0273,
+			">=1.32.0-rancher1-1 <1.32.8-rancher1-1":  flannelv0264,
+			">=1.31.12-rancher1-1 <1.32.0-rancher1-1": flannelv0273,
+			">=1.29.0-rancher1-1 <1.31.12-rancher1-1": flannelv0251,
 			">=1.28.6-rancher1-1 <1.29.0-rancher1-1":  flannelv0242,
 			">=1.26.0-rancher1-1 <1.28.6-rancher1-1":  flannelv0214,
 			">=1.25.0-rancher1-1 <1.26.0-rancher1-1":  flannelv0192,
@@ -450,6 +453,7 @@ func getTemplates() map[string]string {
 		flannelv0242:         FlannelTemplateV0_24_2,
 		flannelv0251:         FlannelTemplateV0_25_1,
 		flannelv0264:         FlannelTemplateV0_26_4,
+		flannelv0273:         FlannelTemplateV0_27_3,
 		flannelv18:           FlannelTemplate,
 
 		canalv113:                     CanalTemplateV113,
