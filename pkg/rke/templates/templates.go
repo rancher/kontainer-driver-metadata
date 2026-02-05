0d478ec686da011c4950eccc1ddcf3233bf7c567
swastik959 <swastik.gour@suse.com>
2026-02-05 15:37:51 +0530

    [dev-v2.11-rke-extended-life] added ingress-nginx v1.13 and 1.14
    Signed-off-by: swastik959 <swastik.gour@suse.com>

--- pkg/rke/templates/templates.go
+++ pkg/rke/templates/templates.go
@@ -142,6 +142,7 @@ const (
 	nginxIngressv1112Rancher2 = "nginxingress-v1.11.2-rancher2"
 	nginxIngressv1113Rancher1 = "nginxingress-v1.11.3-rancher1"
 	nginxIngressv1120Rancher1 = "nginxingress-v1.12.0-rancher1"
+	nginxIngressv1140Rancher1 = "nginxingress-v1.14.0-rancher1"
 
 	nodelocalv115 = "nodelocal-v1.15"
 	nodelocalv121 = "nodelocal-v1.21"
@@ -403,7 +404,8 @@ func LoadK8sVersionedTemplates() map[string]map[string]string {
 			">=1.30.4-rancher1-1 <1.30.5-rancher1-1":   nginxIngressv1112Rancher1,
 			">=1.30.5-rancher1-1 <1.30.6-rancher1-1":   nginxIngressv1112Rancher2,
 			">=1.30.6-rancher1-1 <1.31.4-rancher1-1":   nginxIngressv1113Rancher1,
-			">=1.31.4-rancher1-1 ":                     nginxIngressv1120Rancher1,
+			">=1.31.4-rancher1-1 <1.32.x-rancher1-1":   nginxIngressv1120Rancher1,
+			">=1.32.x-rancher1-1":                      nginxIngressv1140Rancher1,
 		},
 		kdm.Nodelocal: {
 			">=1.15.11-rancher0 <1.16.0-alpha":     nodelocalv115,
@@ -545,6 +547,7 @@ func getTemplates() map[string]string {
 		nginxIngressv1112Rancher2: NginxIngressTemplateV1112Rancher2,
 		nginxIngressv1113Rancher1: NginxIngressTemplateV1113Rancher1,
 		nginxIngressv1120Rancher1: NginxIngressTemplateV1120Rancher1,
+		nginxIngressv1140Rancher1: NginxIngressTemplateV1140Rancher1,
 
 		nodelocalv115: NodelocalTemplateV115,
 		nodelocalv121: NodelocalTemplateV121,
