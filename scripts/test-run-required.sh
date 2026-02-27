4963efcc8633a3d1e48d3701ac7b66e3cdd1edef
Jacob Lindgren <jacob.lindgren@suse.com>
2026-02-27 10:12:03 -0600

    remove unneeded mounts due to dapper being dapper

--- scripts/test-run-required.sh
+++ scripts/test-run-required.sh
@@ -28,5 +28,4 @@ if [ "${GITHUB_EVENT_NAME}" = "push" ] || [ "${GITHUB_EVENT_NAME}" = "pull_reque
 fi
 
 echo "Skipping CI, no changes detected for relevant minor version"
-touch /tmp/out.txt
 exit 1
